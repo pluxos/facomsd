1e8ef62ab5d94b7224a4acf5a5043aca4fc9ce34
kaique <kaiqueribeiro.01@hotmail.com>
2018-09-19 00:36:16 -0300

    teste projeto em branco

---
+++ trabalhoSD/src/main/java/facomSD/facom/com/App.java
@@ -0,0 +1,13 @@
+package facomSD.facom.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
