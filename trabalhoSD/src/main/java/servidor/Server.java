4cad6d760363b348f3789e2bde0c23ed0445c63c
kaique <kaiqueribeiro.01@hotmail.com>
2018-09-23 21:13:20 -0300

    recovery

--- trabalhoSD/src/main/java/servidor/Server.java
+++ trabalhoSD/src/main/java/servidor/Server.java
@@ -2,6 +2,7 @@ package servidor;
 
 import java.net.ServerSocket;
 
+import servidor.dataBase.Data;
 import servidor.queue.Queue;
 import servidor.queue.QueueCommand;
 import utils.Constant;
@@ -11,12 +12,14 @@ public class Server {
 	private ServerSocket serverSocket = null;
 	private QueueCommand queueCommand = null;
 	private Queue queue = null;
+	
 	// QueueCommand queue = new QueueCommand();
 
 	public void iniciar() {
 		try {
 			serverSocket = new ServerSocket(Constant.SERVER_PORT);
 			queueCommand = new QueueCommand();
+			Data.recovery();
 			queue = new Queue(queueCommand);
 			queue.run();
 
