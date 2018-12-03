a744c71356535797a02176a3a3f92da4add48261
vanessafso <vanessasouza@algartelecom.com.br>
2018-12-03 20:27:12 -0200

    Adicionado as correções

--- client.py
+++ client.py
@@ -1,100 +1,143 @@
-# coding: utf-8
-from threading import Thread, Event
-import socket
-import sys
+import grpc
+import grpcFiles.database_pb2 as database_pb2
+import grpcFiles.database_pb2_grpc as database_pb2_grpc
+import os
+from threading import Thread
 import time
 
-HOST = 'localhost'
-PORT = 10001
-BUFFER_SIZE = 1024
-EVENT = Event()
-SOCKET_CONN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SOCKET_CONN.connect((HOST, PORT))
-LOCK = False
 
+class Client:
 
-def show_menu():
-    print('Digite um dos comandos a seguir:\n')
-    print('[1] - Listar registros')
-    print('[2] - Adicionar registro')
-    print('[3] - Atualizar registro')
-    print('[4] - Excluir registro')
-    print('[5] - Sair')
+    def __init__(self):
+        
+        self.instantiate_dirs()
+        self.get_connection()
+        
+        channel = grpc.insecure_channel(self.host+':'+self.port) 
+        self.stub = database_pb2_grpc.DatabaseOperationStub(channel)
 
+    def get_connection(self):
 
-def read_info(command):
-    key = ''
-    value = ''
-    instruction = ''
+        if self.hasConfig():
+            with open ("./configs/config_server", 'r') as fd:
+                data = fd.read()
+                data = data.split('\n')
+                aux = data[0].split(':')
+                self.host = aux[1]
+                aux = data[1].split(':')
+                self.port = aux[1]
+                return True
+        else:
+            print("Error, missing server config")
+            return False
 
-    if command == '1':
-        key = input('Digite a chave a ser lida: ')
-        instruction = 'select'
+    
+    def hasConfig(self):
+        '''Tests if there's a config, or create a default one'''
 
-    elif command == '2':
-        key = input('Digite a chave a ser inserida: ')
-        value = input('Digite o valor a ser inserido: ')
-        instruction = 'insert'
-    elif command == '3':
-        key = input('Digite a chave a ser alterada: ')
-        value = input('Digite o novo valor: ')
-        instruction = 'update'
-    elif command == '4':
-        key = input('Digite a chave a ser removida: ')
-        instruction = 'delete'
+        file_name = "config_client"
+        dir_list = os.listdir('./configs/')
 
-    return (instruction, key, value)
+        if file_name in dir_list:
+            return True
+        else:
+            try:
+                with open('./configs/config_client', 'w') as fd:
+                    data = '''Host:localhost
+Porta:50050'''
+                    fd.write(data)
+                    print("Warning - new config created")
+                    return True
+            except:
+                print("ERROR - can't connect")
+                print("Please, try again")
+                return False
 
+    def instantiate_dirs(self):
+        '''If does not exists, create a dir for logs,snaps, configs'''
 
-def receiver():
-    global LOCK
-    while not EVENT.isSet():
-        msg = SOCKET_CONN.recv(BUFFER_SIZE).decode()  # 1024 é a quantidade de bytes que vai ser lido
-        if not msg:  # se o servidor cair ele retorna uma msg vazia
-            EVENT.set()  # seta o evento para true e o while pára
-            break
-        print(msg)
-        print('\n')
-        LOCK = False
+        dir_list = os.listdir('.')
+        if 'configs' not in dir_list:
+            os.mkdir('configs')
 
+    def sendThread(self):
 
-def sender():
-    global LOCK
-    # se o evento não estiver setado
-    while not EVENT.isSet():
-        if not LOCK:
-            show_menu()
-            command = input()
+        while True:
+            print("-----MENU-------\n")
+            print("1 - create data")
+            print("2 - search data")
+            print("3 - update data")
+            print("4 - delete data")
+            print("5 - quit system")
+            response = ""
+            resposta = input("Digite a opcao:")
+            resposta = resposta.lower()
 
-            if command == "5":
-                # seta o evento com true
-                EVENT.set()
+            if resposta == "sair" or resposta == '5':
+                print("saindo...")
                 break
-            # verifica se é um comando válido
-            elif command in ('1', '2', '3', '4'):
-                instruction, key, value = read_info(command)
-                # envia o comando para o servidor através do socket
-                SOCKET_CONN.send('{0}/{1}/{2}'.format(instruction, key, value).encode())
-                LOCK = True
-            else:
-                print("Comando inválido, tente novamente...")
+        
+            elif resposta == 'create' or resposta == '1':
+                key = input("Digite a chave:")
+                if not key.isdecimal() :
+                    print("A chave deve ser um número inteiro!")
+                
+                else:
+                    value = input("Digite os dados:")
+                    response = self.stub.Create.future(database_pb2.CrUpRequest(chave=key, dados=value))
+                    response.add_done_callback(self.receiveThread)
+    
+            
+            elif resposta == 'read' or resposta == '2':
+                key = input("Digite a chave:")
+                if not key.isdecimal :
+                    print("A chave deve ser um número inteiro!")
+                else:
+                    response = self.stub.Read.future(database_pb2.RdDelRequest(chave=key))
+                    response.add_done_callback(self.receiveThread)
+            
+            elif resposta == 'update' or resposta == '3':
+                key = input("Digite a chave:")
+                if not key.isdecimal() :
+                    print("A chave deve ser um número inteiro!")
+                else:  
+                    value = input("Digite o novo dado:")
+                    response = self.stub.Update.future(database_pb2.CrUpRequest(chave=key,dados=value))
+                    response.add_done_callback(self.receiveThread)
+                
+            elif resposta == 'delete' or resposta == '4':
+                key = input("Digite a chave:")
+                if not key.isdecimal() :
+                    print("A chave deve ser um número inteiro!")
+                else :
+                    response = self.stub.Delete.future(database_pb2.RdDelRequest(chave=key))
+                    response.add_done_callback(self.receiveThread)
+                    
+            else :
+                print("Opcao Invalida")
+            
+    
+    def receiveThread(self,responseFuture):
+        print(responseFuture.result())
 
+    def run(self):
+        
+        masterThread = Thread(target=self.sendThread)
+        
+        masterThread.start()
 
-if __name__ == "__main__":
-    # Verifica se a execução do python começou nesse arquivo
+        masterThread.join()
 
-    t1 = Thread(target=receiver)
-    t1.setDaemon(True)
-    t1.start()
-
-    t2 = Thread(target=sender)
-    t2.setDaemon(True)
-    t2.start()
-
-    t2.join()
-
-    if t1.isAlive():
         time.sleep(5)
-        print("Encerrando execução.")
-    else:
-        print("Servidor desconectado... Encerrando execução.")
+        
+        print('Quit client')
+        
+        
+
+
+
+        
+
+if __name__ == '__main__':
+    meu_cliente = Client()
+    meu_cliente.run()
