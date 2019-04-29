512c6a299b7e101ba0b3d6e5383a165bc7cd5f06
matheussn <matheusneto23@gmail.com>
2019-04-29 11:11:56 -0300

    fix imports

--- src/main/java/server/ServerApplication.java
+++ src/main/java/server/ServerApplication.java
@@ -1,8 +1,7 @@
 package server;
+
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
