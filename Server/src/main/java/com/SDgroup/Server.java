74e3890188a2ee6267501379d591887bbd076725
Gustavo Faria (gsilva.00@hotmail.com) <gsilva.00@hotmail.com>
2019-04-29 22:25:41 -0300

    Logical Merge with master

--- Server/src/main/java/com/SDgroup/Server.java
+++ Server/src/main/java/com/SDgroup/Server.java
@@ -1,13 +1,32 @@
 package com.SDgroup;
 
-/**
- * Hello world!
- *
- */
-public class Server
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Properties;
+
+class Server{
+  public static void main(String argv[]) throws Exception {
+    Properties properties = new Properties();
+    FileInputStream propsFS = new FileInputStream("src/main/resources/Constants.prop");
+    properties.load(propsFS);
+    
+    Integer port = Integer.parseInt(properties.getProperty("port"));
+    
+    
+    ServerSocket ssock = new ServerSocket(port);
+    System.out.println("Listening on port " + port);
+    while (true) {
+      // o método accept() bloqueia a execução até que
+      // o servidor receba um pedido de conexão    
+      Socket sock = ssock.accept();
+      System.out.println("Connected");
+      new Thread(new EntryPoint(sock)).start();
     }
+    
+    
+  }
+  
+  
+  
 }
