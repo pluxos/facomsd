6cc71df50b8be9047404544b6a1ec2dc1570c334
João Paulo de Oliveira <joaopaulodepliveira123@gmail.com>
2019-04-30 23:59:54 -0300

    pronto

--- Server/src/main/java/server/Server.java
+++ Server/src/main/java/server/Server.java
@@ -10,11 +10,10 @@ class Server {
 
   public static void main(String[] args) {
     try {
-      // Properties properties = new Properties();
-      // FileInputStream propsFS = new FileInputStream("Server/src/main/resources/Constants.prop");
-      // properties.load(propsFS);
-
-      Integer port = 12345;// Integer.parseInt(properties.getProperty("port"));
+       Properties properties = new Properties();
+       FileInputStream propsFS = new FileInputStream("Server/src/main/resources/Constants.prop");
+       properties.load(propsFS);
+       Integer port = Integer.parseInt(properties.getProperty("port"));
 
       ServerSocket server = new ServerSocket(port);
       System.out.println("Listening on port " + port);
