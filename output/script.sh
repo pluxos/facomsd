53b991000d5a0216f89cd498456c942aab350e4b
João Paulo de Oliveira <joao.oliveira@neppoti.com.br>
2019-05-21 13:44:39 -0300

    Comments on execution script

--- output/script.sh
+++ output/script.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# Creating a default log
 cp log ../Server/src/main/java/server/
-
+# Build the client
 cd ../Client
 mvn clean package
 
+# Build the server
 cd ../Server
 mvn clean package
 
+# Execute one server
 cd ../output
-
 java -jar Server-1.0-SNAPSHOT.jar &
 
+# Execute five clients
 executeClients(){
 	java -jar Client-1.0-SNAPSHOT.jar < in
 }
@@ -22,5 +25,6 @@ do
 	executeClients & # Put the function in background
 done
 
+# Block the terminal until all threads finished
 wait 
 echo "All done"
